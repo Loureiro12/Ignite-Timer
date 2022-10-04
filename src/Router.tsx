773d15c4d4e6430ac61521e30d44bb6d4cc7daf9
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-10-04 16:16:09 -0300

    feat: áginas e rotas - Layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayouts } from './layouts/DefaultLayouts'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayouts />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
