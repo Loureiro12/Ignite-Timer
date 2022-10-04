e0cba23ae2f7687a5188165713766f2566a6760b
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-10-04 17:30:34 -0300

    Páginas e rotas - Finalização da Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Play } from 'phosphor-react'
+import {
+  FormContainer,
+  HomeContainer,
+  CountdownContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê un nome para o seu projeto" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
+          <span>mínutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} /> Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
